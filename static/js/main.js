af576c4cffa6ce04f94aa7b9c1bdb928a1942464
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-09-30 15:47:55 +0200

    change mob movement to coordinate from id

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
-let firstId = document.getElementById("1");
-firstId.addEventListener("click", mobs);
-
+const mobRoute = [[0, 5], [1, 5], [2, 5], [2, 4], [2, 3], [2, 2], [3, 2], [4, 2], [5, 2], [5, 3], [5, 4], [5, 5],
+[5, 6], [6, 6], [7, 6], [8, 6], [9, 6], [9, 5], [9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4]];
+const X = 0;
+const Y = 1;
 
 async function mobs() {
     for (let i = 1; i<6; i++) {
@@ -9,13 +10,21 @@ async function mobs() {
     }
 }
 async function steps() {
-    for (let i = 1; i < 25; ++i) {
-        document.getElementById(i.toString()).style.backgroundColor = "black";
+    for (let coordinate of mobRoute) {
+        let cell = document.querySelector('[data-coordinate-x="'+ coordinate[X] +'"][data-coordinate-y="' + coordinate[Y] +'"]');
+        cell.style.backgroundColor = "black";
         await sleep(500);
-        document.getElementById(i.toString()).style.backgroundColor = "";
+        cell.style.backgroundColor = "";
     }
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function main () {
+    let firstId = document.querySelector('[data-coordinate-x="0"][data-coordinate-y="0"]');
+    firstId.addEventListener("click", mobs);
+}
+
+main();
